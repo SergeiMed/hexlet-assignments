9b3494a4ccda0ddd5e8af6ed2cfce4afef9bc505
@hexlet/cli <support@hexlet.io>
2022-10-19 23:33:39 +0300

    submit java-oop-ru/inheritance

--- java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -1,9 +1,32 @@
 package exercise;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
-import java.util.stream.Collectors;
 
 // BEGIN
+public class PairedTag extends Tag{
 
+    private String tagText;
+    List<Tag> listOfChildren;
+
+    public PairedTag(String tagName, Map<String, String> tagAttributes, String tagText, List<Tag> listOfChildren) {
+        super(tagName, tagAttributes);
+        this.tagText = tagText;
+        this.listOfChildren = new ArrayList<>(listOfChildren);
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder(getTag());
+        stringBuilder.append(tagText);
+        for (Tag tag : listOfChildren) {
+            stringBuilder.append((new SingleTag(tag.getTagName(), tag.getTagAttributes()).getTag()));
+
+        }
+        stringBuilder.append("</")
+                .append(getTagName())
+                .append(">");
+        return stringBuilder.toString();
+    }
+}
 // END
--- java-oop-ru/inheritance/src/main/java/exercise/Tag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/Tag.java
@@ -1,8 +1,46 @@
 package exercise;
 
-import java.util.stream.Collectors;
 import java.util.Map;
 
 // BEGIN
+public abstract class Tag {
 
+    private String tagName;
+    private Map<String, String> tagAttributes;
+
+    public Tag(String tagName, Map<String, String> tagAttributes) {
+        this.tagName = tagName;
+        this.tagAttributes = tagAttributes;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public Map<String, String> getTagAttributes() {
+        return tagAttributes;
+    }
+
+    public String getTag() {
+        if (tagAttributes.isEmpty()) {
+            return "<" + tagName + ">";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, String> attribute : tagAttributes.entrySet()) {
+            stringBuilder.append(attribute.getKey())
+                    .append("=")
+                    .append("\"")
+                    .append(attribute.getValue())
+                    .append("\"")
+                    .append(" ");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        String result = stringBuilder.toString();
+        return getTagAttributes().isEmpty() ? "<" + getTagName() + ">" :
+                "<" + getTagName() +
+                        " " +
+                        result +
+                        ">";
+    }
+}
 // END
